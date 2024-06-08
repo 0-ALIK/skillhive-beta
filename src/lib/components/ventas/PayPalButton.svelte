d8ae95fb8ff3c3bf65228d06557035c1795c6d3a
Alik <moisesalik01@gmail.com>
2024-06-08 18:04:13 -0500

    empezando paypal y arreglos en home

---
+++ src/lib/components/ventas/PayPalButton.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { loadScript } from '@paypal/paypal-js';
+
+    const clientId = 'AWR0qNtqhumda4krctE9stYtkIVikYsgk5Kz3-Ip0-pAsUH7ZO6AlOLlWBnPikFDpn7KHubW_7ho-UoM';
+
+    loadScript({clientId: clientId}).then(( paypal ) => {
+        paypal 
+            .Buttons({
+                fundingSource: paypal.FUNDING.PAYPAL,
+            })
+            .render('#paypal-button-container')
+            .catch((error) => {
+                console.error('Error loading PayPal SDK:', error);
+            });
+    })
+    .catch((error) => {
+        console.error('Error loading PayPal SDK:', error);
+    });
+</script>
+
+<div id="paypal-button-container"></div>
